701426c040e2e0a8db92bf3aadfeab898f01ef3e
arunbalajeesv-1 <arunbalajeesv@gmail.com>
2026-06-12 22:42:16 +0530

    Delete GST Fix

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getCustomer, updateDeliveryAddress, updateRegisteredAddress, updateGSTDetails, removeGSTDetails, listCustomers, getCustomerByPhone } = require('../controllers/customerController');
+const authenticate = require('../middleware/auth');
 
 router.get('/', listCustomers);
 router.get('/phone/:phone', getCustomerByPhone);
@@ -8,6 +9,6 @@ router.get('/:userId', getCustomer);
 router.put('/:userId/delivery-address', updateDeliveryAddress);
 router.put('/:userId/registered-address', updateRegisteredAddress);
 router.put('/:userId/gst', updateGSTDetails);
-router.delete('/:userId/gst', removeGSTDetails);
+router.delete('/:userId/gst', authenticate, removeGSTDetails);
 
 module.exports = router;
